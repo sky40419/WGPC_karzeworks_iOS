afbe3d10f0a636a7283b6f24ae8de2f3a1967857
Karze Huang <gash61511@gmail.com>
2025-06-30 01:28:30 +0800

    WGPCiOS_0.1.3_1000130
    - 修正UI自適應問題
    - 修正日曆顯示問題
    - 修正解析度適應問題
    - 新增返回功能（測試中）

---
+++ Il2CppOutputProject/Source/il2cppOutput/Assembly-CSharp__1.cpp
@@ -0,0 +1,95 @@
+﻿#include "pch-cpp.hpp"
+
+
+
+
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+struct U3CPrivateImplementationDetailsU3E_t0F5473E849A5A5185A9F4C5246F0C32816C49FCA  : public RuntimeObject
+{
+};
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F  : public RuntimeObject
+{
+};
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_pinvoke
+{
+};
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_com
+{
+};
+#pragma pack(push, tp, 1)
+struct __StaticArrayInitTypeSizeU3D1865_t4F72AEB5905BFC0ED736AF5B4987D894BEB2109D 
+{
+	union
+	{
+		struct
+		{
+			union
+			{
+			};
+		};
+		uint8_t __StaticArrayInitTypeSizeU3D1865_t4F72AEB5905BFC0ED736AF5B4987D894BEB2109D__padding[1865];
+	};
+};
+#pragma pack(pop, tp)
+#pragma pack(push, tp, 1)
+struct __StaticArrayInitTypeSizeU3D2037_t1BE9B94E0BF50DF37C7BFA8D4E2802B38C29D5B4 
+{
+	union
+	{
+		struct
+		{
+			union
+			{
+			};
+		};
+		uint8_t __StaticArrayInitTypeSizeU3D2037_t1BE9B94E0BF50DF37C7BFA8D4E2802B38C29D5B4__padding[2037];
+	};
+};
+#pragma pack(pop, tp)
+struct U3CPrivateImplementationDetailsU3E_t0F5473E849A5A5185A9F4C5246F0C32816C49FCA_StaticFields
+{
+	__StaticArrayInitTypeSizeU3D1865_t4F72AEB5905BFC0ED736AF5B4987D894BEB2109D ___2250DD85BF5F8D6D8599179B9148AF6255D1E157EE05FB5188D081D7BA56ABAF;
+	__StaticArrayInitTypeSizeU3D2037_t1BE9B94E0BF50DF37C7BFA8D4E2802B38C29D5B4 ___544A68FB51DAFB1F297DEC6094122CEAE082B7F4BD653C2533172D62FEDDD45C;
+};
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
